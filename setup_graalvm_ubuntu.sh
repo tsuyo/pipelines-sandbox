a778a9c65bf33b556fe87db8850b0347d566e8e2
Tsuyoshi Miyake <tsuyoshi.miyake@gmail.com>
2020-10-17 15:39:39 +0900

    modify graalvm download url

--- setup_graalvm_ubuntu.sh
+++ setup_graalvm_ubuntu.sh
@@ -1,6 +1,6 @@
 sudo apt-get update
 sudo apt-get install gcc zlib1g-dev build-essential unzip
-wget https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-18.2.0/graalvm-ce-java11-linux-amd64-20.2.0.tar.gz
+wget https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-20.2.0/graalvm-ce-java11-linux-amd64-20.2.0.tar.gz
 tar zxf graalvm-ce-java11-linux-amd64-20.2.0.tar.gz
 sudo mv graalvm-ce-java11-20.2.0 /usr/lib/jvm
 echo 'export PATH=/usr/lib/jvm/graalvm-ce-java11-20.2.0/bin:$PATH' >> ~/.bashrc
